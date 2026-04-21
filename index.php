c6fc70d53868ed3c75fd6e8c1d0fce967f04b119
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-21 20:08:16 +0530

    add hero section

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
-
+<!-- Start Main Section -->
+<main class="site-content" id="content">
+<?php
+get_template_part('template-parts/hero');
+?>
+</main>
+<!-- End Main Section -->
 <?php get_footer(); ?>
---
+++ template-parts/hero.php
@@ -0,0 +1,110 @@
+<!-- start: Hero Area -->
+<section class="hero-section style-10">
+<div class="bg-shape">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-shape.png" alt="img">
+</div>
+<div class="intro_text">
+<svg viewBox="0 0 1320 300">
+<text x="50%" Y="50%" text-anchor="middle">HI</text>
+</svg>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-lg-6">
+<div class="hero-content-box style-10 tj-hero-7-content">
+<div class="subtitle"><img class="wave" src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-h8-1.png" alt="Icons"> Hi There, I am <span>Gerold</span></div>
+<h1 class="hero-title">Let’s Unlocking Potential Best Delivering.</h1>
+<div class="desc">
+<p>IT consultants assist organizations in migrating to cloud-based services,</p>
+</div>
+<div class="hero-button">
+<a href="#" class="btn tj-btn-primary">Download CV <i class="fa-solid fa-download"></i></a>
+<a href="#" class="btn tj-btn-secondary">Got a project? <i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+</div>
+<div class="col-lg-6">
+<div class="hero-10-images">
+<div class="hero-image-box text-center">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/me.png" alt="">
+</div>
+<div class="hero-10-reviews-wrap tj-counter-5-shape-2">
+<div class="hero-10-thumb">
+<ul>
+<li><img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-4-review-img1.png" alt="Images"></li>
+<li><img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-4-review-img2.png" alt="Images"></li>
+<li><img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-4-review-img3.png" alt="Images"></li>
+<li class="count"><span>2K</span></li>
+</ul>
+</div>
+<div class="hero-8-reviews-rating">
+<div class="testimonial-rating">
+<div class="star-ratings" style="width: 139.969px;">
+<div class="fill-ratings" style="width: 86%">
+<span>★★★★★</span>
+</div>
+<div class="empty-ratings">
+<span>★★★★★</span>
+</div>
+</div>
+</div>
+<p><span>1.8K+</span> 5 Star Reviews</p>
+</div>
+</div>
+<div class="hero-8-chats tj-counter-5-shape-1"><span><i class="fa-solid fa-question"></i></span>Need Consultation?</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="funfact-area">
+<div class="row">
+<div class="col-6 col-lg-3">
+<div class="funfact-item d-flex flex-column flex-wrap align-items-center">
+<div class="number"><span class="odometer odometer-auto-theme" data-count="14"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">1</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">4</span></span></span></span></span></div></span></div>
+<div class="text">Years of Experience</div>
+</div>
+</div>
+<div class="col-6 col-lg-3">
+<div class="funfact-item d-flex flex-column flex-wrap align-items-center">
+<div class="number"><span class="odometer odometer-auto-theme" data-count="50"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">5</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">0</span></span></span></span></span></div></span>+</div>
+<div class="text">Project Completed</div>
+</div>
+</div>
+<div class="col-6 col-lg-3">
+<div class="funfact-item d-flex flex-column flex-wrap align-items-center">
+<div class="number"><span class="odometer odometer-auto-theme" data-count="1.5"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">1</span></span></span></span></span><span class="odometer-formatting-mark odometer-radix-mark">.</span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">5</span></span></span></span></span></div></span>K</div>
+<div class="text">Happy Clients</div>
+</div>
+</div>
+<div class="col-6 col-lg-3">
+<div class="funfact-item d-flex flex-column flex-wrap align-items-center">
+<div class="number"><span class="odometer odometer-auto-theme" data-count="14"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">1</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">4</span></span></span></span></span></div></span></div>
+<div class="text">Years of Experience</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="hero_scroll">
+<a class="down" href="#scroll-hero"><span><i class="fa-solid fa-arrow-right"></i></span>Scroll Down
+</a>
+</div>
+<div class="hero-10-socials">
+<ul class="ul-reset social-icons">
+<li>
+<a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+</li>
+<li>
+<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+</li>
+<li>
+<a href="#"><i class="fa-brands fa-facebook"></i></a>
+</li>
+<li>
+<a href="#"><i class="fa-brands fa-instagram"></i></a>
+</li>
+</ul>
+</div>
+</section>
+<!-- end: Hero Area -->
