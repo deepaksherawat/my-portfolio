274c3d43a97d18f2ee111e4442bf73de8d9db7d5
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-21 19:18:22 +0530

    add footer

--- footer.php
+++ footer.php
@@ -1,3 +1,64 @@
+<!-- start: Footer Area -->
+<div class="main-footer">
+<div class="bg-shape">
+<img src="http://deepakwebdeveloper.42web.io/wp-content/themes/deepaksherawat/assets/images/bg-shape.png" alt="img">
+</div>
+<!-- start: Text Area -->
+<section class="text-section">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="section-header">
+<div class="heading-left">
+<p class="wow fadeInUp" data-wow-delay=".3s"><a class="link modal-popup" href="#contact-wrapper">Want to start a project?</a></p>
+<a class="link modal-popup" href="#contact-wrapper"><h2 id="anim" class="section-title wow fadeInUp" data-wow-delay=".4s">Let’s have a chat</h2></a>
+</div>
+<div class="chat-mail wow fadeInRight" data-wow-delay=".5s">
+<a class="link modal-popup" href="#contact-wrapper">info@taylor.com <i class="fa-solid fa-arrow-right"></i></a>
+<!-- <a class="btn tj-btn-primary modal-popup" href="#service-wrapper">Learn More <i class="fa-solid fa-arrow-right"></i></a> -->
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- end: Text Area -->
+<section class="tj-footer-6-area">
+<div class="container">
+<div class="row">
+<div class="col">
+<div class="tj-footer-6-wrapper">
+<div class="tj-footer-6-left">
+<p class="tj-footer-6-paragraph">AVAILABLE FOR FREELANCE</p>
+</div>
+<div class="tj-footer-6-middle">
+<div class="footer-menu tj-footer-6-menu">
+<nav>
+<ul class="desktop_social_icon">
+<li><a href="portfolio.html"><i class="fa-brands fa-whatsapp"></i>WhatsApp.</a></li>
+<li><a href="services.html"><i class="fa-brands fa-linkedin-in"></i>Linkedin.</a></li>
+<li><a href="about.html"><i class="fa-brands fa-facebook"></i>Facebook.</a></li>
+<li><a href="contact.html"><i class="fa-brands fa-instagram"></i>Instagram.</a></li>
+</ul>
+<ul class="mobile_social_icon">
+<li><a href="portfolio.html"><i class="fa-brands fa-whatsapp"></i></a></li>
+<li><a href="services.html"><i class="fa-brands fa-linkedin-in"></i></a></li>
+<li><a href="about.html"><i class="fa-brands fa-facebook"></i></a></li>
+<li><a href="contact.html"><i class="fa-brands fa-instagram"></i></a></li>
+</ul>
+</nav>
+</div>
+</div>
+<div class="tj-footer-6-right">
+<p class="tj-footer-6-copyright">© All rights reserved by <a href="index.html">Deepak Sherawat Web developer</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!-- end: Footer Area -->
+
 <!-- start: Footer Contact Popup -->
 <div id="contact-wrapper" class="popup_content_area zoom-anim-dialog mfp-hide" data-lenis-prevent>
 <div class="popup_modal_content">
