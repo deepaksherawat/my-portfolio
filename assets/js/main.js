3814f8bf4f907305a2caf18fe06d52d91afc3e00
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-22 15:42:28 +0530

    fix console

--- assets/js/main.js
+++ assets/js/main.js
@@ -997,15 +997,15 @@ Description: Gerold - Personal Portfolio HTML5 Template
 		}
 
 		// Fun Fact Js
-		if ($(".odometer").length > 0) {
-			$(".odometer").appear(function () {
-				var odo = $(".odometer");
-				odo.each(function () {
-					var countNumber = $(this).attr("data-count");
-					$(this).html(countNumber);
-				});
-			});
-		}
+		if ($(".odometer").length > 0 && $.fn.appear) {
+            $(".odometer").appear(function () {
+            var odo = $(".odometer");
+            odo.each(function () {
+            var countNumber = $(this).attr("data-count");
+           $(this).html(countNumber);
+           });
+          });
+          }
 
 		// Side Bar Sticky Js
 		if ($(".side-sticky").length > 0) {
