2da32ed900d9e52380185699f8e318a1cfa2cd50
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-23 17:31:47 +0530

    update blog

--- template-parts/blog-slider.php
+++ template-parts/blog-slider.php
@@ -33,6 +33,7 @@
 <?php
 $args = array(
     'post_type' => 'blog',
+    'posts_per_page' => -1
 );
 $query = new WP_Query($args);
 if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
