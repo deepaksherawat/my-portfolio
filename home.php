222d65f368230102e43731d8ab89d41afd7f9864
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-23 20:16:21 +0530

    test

--- home.php
+++ home.php
@@ -7,14 +7,7 @@ get_header();
 <!-- Start Main Section -->
 <main class="site-content" id="content">
 <?php
-get_template_part('template-parts/hero');
-get_template_part('template-parts/service');
-get_template_part('template-parts/portfolio');
-get_template_part('template-parts/resume');
-get_template_part('template-parts/skills');
-get_template_part('template-parts/testimonials');
-get_template_part('template-parts/blog-slider');
-get_template_part('template-parts/faqs');
+
 ?>
 </main>
 <!-- End Main Section -->
