703900485827230e2429939cd78bec86bc8d75a4
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-23 20:48:52 +0530

    testing

--- home.php
+++ home.php
@@ -9,8 +9,11 @@ get_header();
 <?php
 get_template_part('template-parts/hero');
 get_template_part('template-parts/service');
-get_template_part('template-parts/portfolio');
 get_template_part('template-parts/resume');
+get_template_part('template-parts/skills');
+get_template_part('template-parts/testimonials');
+get_template_part('template-parts/blog-slider');
+get_template_part('template-parts/faqs');
 
 ?>
 </main>
