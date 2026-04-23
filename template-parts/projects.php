4ab15e3e07259650a6b2dde70845d03ca73157b3
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-23 20:54:12 +0530

    add pro

--- template-parts/projects.php
+++ template-parts/projects.php
@@ -1,1 +1,146 @@
-<h1>Project Section</h1>
+<!-- start: Portfolio Area -->
+<section class="portfolio-section style-5">
+<div class="bg-shape">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-shape.png" alt="img">
+</div>
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="section-header style-3 portfolio_nav_desktop">
+<div class="sec-text">
+<span class="subtitle wow fadeInLeft" data-wow-delay=".3s">Behind the Pixels</span>
+<h2 class="title">My Latest Projects</h2>
+</div>
+<div class="portfolio-navigation d-none d-lg-inline-flex wow fadeInRight" data-wow-delay=".4s">
+<div class="portfolio-prev"><i class="fa-solid fa-arrow-left"></i></div>
+<div class="portfolio-next"><i class="fa-solid fa-arrow-right"></i></div>
+</div>
+<div class="portfolio-button wow fadeInRight" data-wow-delay=".5s">
+<a class="btn tj-btn-primary" href="#">View All Project <i class="fa-solid fa-arrow-right"></i></a>
+</div>
+<div class="portfolio-shapes">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/h5-progress-2.png" alt="">
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container portfolio_slider">
+<div class="row">
+                    <div class="col-12">
+                        <div class="swiper portfolio-slider-5">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <div class="portfolio-item style-5 wow fadeInUp" data-wow-delay=".3s">
+                                        <div class="image-box">
+                                            <a class="modal-popup" href="#portfolio-wrapper"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-1.jpg" alt="Images" /></a>
+                                        </div>
+                                        <div class="content-box">
+                                            <div class="portfolio-text">
+                                                <h5 class="portfolio-title"><a class="modal-popup"
+                                                        href="#portfolio-wrapper">Deloitte</a></h5>
+                                                <p>Project was about precision and information.</p>
+                                            </div>
+                                            <div class="portfolio-arrow">
+                                                <a class="modal-popup" href="#portfolio-wrapper">
+                                                    <span class="icon_box">
+                                                        <i class="icon_first fa-solid fa-arrow-right"></i>
+                                                        <i class="icon_second fa-solid fa-arrow-right"></i>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="portfolio-item style-5 wow fadeInUp" data-wow-delay=".5s">
+                                        <div class="image-box">
+                                            <a class="modal-popup" href="#portfolio-wrapper">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-2.jpg" alt="Images" />
+                                            </a>
+                                        </div>
+                                        <div class="content-box">
+                                            <div class="portfolio-text">
+                                                <h5 class="portfolio-title"><a class="modal-popup"
+                                                        href="#portfolio-wrapper">New Age</a></h5>
+                                                <p>Project was about precision and information.</p>
+                                            </div>
+                                            <div class="portfolio-arrow">
+                                                <a class="modal-popup" href="#portfolio-wrapper">
+                                                    <span class="icon_box">
+                                                        <i class="icon_first fa-solid fa-arrow-right"></i>
+                                                        <i class="icon_second fa-solid fa-arrow-right"></i>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="portfolio-item style-5 wow fadeInUp" data-wow-delay=".7s">
+                                        <div class="image-box">
+                                            <a class="modal-popup" href="#portfolio-wrapper">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-3.jpg" alt="Images" />
+                                            </a>
+                                        </div>
+                                        <div class="content-box">
+                                            <div class="portfolio-text">
+                                                <h5 class="portfolio-title"><a class="modal-popup"
+                                                        href="#portfolio-wrapper">Sebastian</a></h5>
+                                                <p>Project was about precision and information.</p>
+                                            </div>
+                                            <div class="portfolio-arrow">
+                                                <a class="modal-popup" href="#portfolio-wrapper">
+                                                    <span class="icon_box">
+                                                        <i class="icon_first fa-solid fa-arrow-right"></i>
+                                                        <i class="icon_second fa-solid fa-arrow-right"></i>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="portfolio-item style-5 wow fadeInUp" data-wow-delay=".9s">
+                                        <div class="image-box">
+                                            <a class="modal-popup" href="#portfolio-wrapper">
+                                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/work-1.jpg" alt="Images" />
+                                            </a>
+                                        </div>
+                                        <div class="content-box">
+                                            <div class="portfolio-text">
+                                                <h5 class="portfolio-title"><a class="modal-popup"
+                                                        href="#portfolio-wrapper">Deloitte</a></h5>
+                                                <p>Project was about precision and information.</p>
+                                            </div>
+                                            <div class="portfolio-arrow">
+                                                <a class="modal-popup" href="#portfolio-wrapper">
+                                                    <span class="icon_box">
+                                                        <i class="icon_first fa-solid fa-arrow-right"></i>
+                                                        <i class="icon_second fa-solid fa-arrow-right"></i>
+                                                    </span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="portfolio-pagination"></div>
+                        </div>
+                        <div class="portfolio_nav_mobile">
+<div class="portfolio-navigation wow fadeInRight" data-wow-delay=".4s">
+<div class="portfolio-prev"><i class="fa-solid fa-arrow-left"></i></div>
+<div class="portfolio-next"><i class="fa-solid fa-arrow-right"></i></div>
+</div>
+<div class="portfolio-button wow fadeInRight" data-wow-delay=".5s">
+<a class="btn tj-btn-primary" href="#">View All Project <i class="fa-solid fa-arrow-right"></i></a>
+</div>
+</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end: Portfolio Area -->
+<!-- start: Portfolio Popup -->
+
+<!-- end: Portfolio Popup -->
