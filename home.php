9330c8aee7635d24f1f2520df777910b4de26fd8
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-23 20:24:07 +0530

    upload hero section

--- home.php
+++ home.php
@@ -8,13 +8,7 @@ get_header();
 <main class="site-content" id="content">
 <?php
 get_template_part('template-parts/hero');
-get_template_part('template-parts/service');
-get_template_part('template-parts/portfolio');
-get_template_part('template-parts/resume');
-get_template_part('template-parts/skills');
-get_template_part('template-parts/testimonials');
-get_template_part('template-parts/blog-slider');
-get_template_part('template-parts/faqs');
+
 ?>
 </main>
 <!-- End Main Section -->
