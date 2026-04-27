2b627421738419993b57224a4836193d1a9deb22
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-27 21:06:56 +0530

    popup under loop

--- template-parts/projects.php
+++ template-parts/projects.php
@@ -43,7 +43,7 @@ while($query->have_posts()) : $query->the_post();
 <div class="swiper-slide">
 <div class="portfolio-item style-5 wow fadeInUp" data-wow-delay=".9s">
 <div class="image-box">
-<a class="modal-popup" href="#portfolio-wrapper-<?php echo get_the_ID(); ?>">
+<a class="modal-popup" href="#portfolio-wrapper">
 <?php 
 if ( has_post_thumbnail() ) {
 $img = get_the_post_thumbnail_url(get_the_ID(), 'full');
@@ -54,11 +54,11 @@ echo '<img src="'.$img.'" alt="'.get_the_title().'">';
 </div>
 <div class="content-box">
 <div class="portfolio-text">
-<h5 class="portfolio-title"><a class="modal-popup" href="#portfolio-wrapper-<?php echo get_the_ID(); ?>"><?php the_title(); ?></a></h5>
+<h5 class="portfolio-title"><a class="modal-popup" href="#portfolio-wrapper"><?php the_title(); ?></a></h5>
 <p><?php the_category(', '); ?></p>
 </div>
 <div class="portfolio-arrow">
-<a class="modal-popup" href="#portfolio-wrapper-<?php echo get_the_ID(); ?>">
+<a class="modal-popup" href="#portfolio-wrapper">
 <span class="icon_box">
 <i class="icon_first fa-solid fa-arrow-right"></i>
 <i class="icon_second fa-solid fa-arrow-right"></i>
@@ -68,6 +68,102 @@ echo '<img src="'.$img.'" alt="'.get_the_title().'">';
 </div>
 </div>
 </div>
+
+<!-- start: Portfolio Popup -->
+<div id="portfolio-wrapper" class="popup_content_area zoom-anim-dialog mfp-hide" data-lenis-prevent>
+<div class="popup_modal_content">
+<div class="portfolio_info">
+<div class="portfolio_info_text">
+<h2 class="title"><?php the_title(); ?></h2>
+<!-- <div class="desc">
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered  alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+</div> -->
+<a href="#" class="btn tj-btn-primary">live preview <i class="fa-solid fa-arrow-right"></i></a>
+</div>
+<div class="portfolio_info_items">
+<?php if( get_field('project_name') ): ?>
+<div class="info_item">
+<div class="key">Project Name</div>
+<div class="value"><?php the_field('project_name'); ?></div>
+</div>
+<?php endif; ?>
+<div class="info_item">
+<div class="key">Client</div>
+<div class="value">Artboard Studio</div>
+</div>
+<div class="info_item">
+<div class="key">Start Date</div>
+<div class="value">August 20, 2023</div>
+</div>
+<div class="info_item">
+<div class="key">Designer</div>
+<div class="value"><a href="#">ThemeJunction</a></div>
+</div>
+</div>
+</div>
+<div class="portfolio_gallery owl-carousel">
+<div class="gallery_item">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/p-gallery-1.jpg" alt="" />
+</div>
+<div class="gallery_item">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/p-gallery-2.jpg" alt="" />
+</div>
+<div class="gallery_item">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/p-gallery-3.jpg" alt="" />
+</div>
+<div class="gallery_item">
+<img src="<?php echo get_template_directory_uri(); ?>/assets/images/p-gallery-4.jpg" alt="" />
+</div>
+</div>
+<div class="portfolio_description">
+<h2 class="title">Project Description</h2>
+<div class="desc">
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered  alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered  alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+</div>
+</div>
+<div class="portfolio_story_approach">
+<div class="portfolio_story">
+<div class="story_title">
+<h4 class="title">The story</h4>
+</div>
+<div class="story_content">
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered  alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+</div>
+</div>
+<div class="portfolio_approach">
+<div class="approach_title">
+<h4 class="title">OUR APPROACH</h4>
+</div>
+<div class="approach_content">
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered  alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+</div>
+</div>
+</div>
+<div class="portfolio_navigation">
+<div class="navigation_item prev-project">
+<a href="#" class="project">
+<i class="fa-solid fa-arrow-right"></i>
+<div class="nav_project">
+<div class="label">Previous Project</div>
+<h3 class="title">Sebastian</h3>
+</div>
+</a>
+</div>
+<div class="navigation_item next-project">
+<a href="#" class="project">
+<div class="nav_project">
+<div class="label">Next Project</div>
+<h3 class="title">Qwillo</h3>
+</div>
+<i class="fa-solid fa-arrow-right"></i>
+</a>
+</div>
+</div>
+</div>
+</div>
+<!-- end: Portfolio Popup -->
+
 <?php endwhile; wp_reset_postdata(); endif; ?>
 </div>
 <div class="portfolio-pagination"></div>
@@ -86,6 +182,3 @@ echo '<img src="'.$img.'" alt="'.get_the_title().'">';
 </div>
 </section>
 <!-- end: Portfolio Area -->
-<!-- start: Portfolio Popup -->
-<?php get_template_part('template-parts/popup-project-detail'); ?>
-<!-- end: Portfolio Popup -->
