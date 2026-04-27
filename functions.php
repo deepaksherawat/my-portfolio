56650a4fc5b2b20de99b17a4f385356148d74634
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-27 15:13:25 +0530

    add theme setting

--- functions.php
+++ functions.php
@@ -118,4 +118,17 @@ add_theme_support('post-thumbnails');
 
 add_image_size('blog-card', 409, 368, true); // true = hard crop
 
+
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+        'page_title'  => 'Theme Settings',
+        'menu_title'  => 'Theme Settings',
+        'menu_slug'   => 'theme-settings',
+        'capability'  => 'edit_posts',
+        'redirect'    => false
+    ));
+
+}
+
 ?>
