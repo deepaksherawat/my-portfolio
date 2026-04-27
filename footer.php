1b34038ad03e0fd1e494d0197dd576fada801c92
deepaksherawat <deepaksherawat34@gmail.com>
2026-04-27 18:32:28 +0530

    footer text

--- footer.php
+++ footer.php
@@ -10,11 +10,11 @@
 <div class="col-12">
 <div class="section-header">
 <div class="heading-left">
-<p class="wow fadeInUp" data-wow-delay=".3s"><a class="link modal-popup" href="#contact-wrapper">Want to start a project?</a></p>
-<a class="link modal-popup" href="#contact-wrapper"><h2 id="anim" class="section-title wow fadeInUp" data-wow-delay=".4s">Let’s have a chat</h2></a>
+<p class="wow fadeInUp" data-wow-delay=".3s"><a class="link modal-popup" href="#contact-wrapper"><?php echo get_field('footer_sub_heading', 'option'); ?></a></p>
+<a class="link modal-popup" href="#contact-wrapper"><h2 id="anim" class="section-title wow fadeInUp" data-wow-delay=".4s"><?php echo get_field('footer_main_heading', 'option'); ?></h2></a>
 </div>
 <div class="chat-mail wow fadeInRight" data-wow-delay=".5s">
-<a class="link modal-popup" href="#contact-wrapper">info@taylor.com <i class="fa-solid fa-arrow-right"></i></a>
+<a class="link modal-popup" href="#contact-wrapper"><?php echo get_field('footer_email_id', 'option'); ?><i class="fa-solid fa-arrow-right"></i></a>
 <!-- <a class="btn tj-btn-primary modal-popup" href="#service-wrapper">Learn More <i class="fa-solid fa-arrow-right"></i></a> -->
 </div>
 </div>
