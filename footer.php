8467c2a64fd2912c786d7d39ada8f01a52938a31
deepaksherawat <deepaksherawat34@gmail.com>
2026-07-16 22:19:47 +0530

    change in popup

--- footer.php
+++ footer.php
@@ -5,9 +5,18 @@ $linkedin_link  =  get_field('linkedin_link', 'option') ?: 'https://www.linkedin
 $instagram_link  =  get_field('instagram_link', 'option') ?: 'https://www.instagram.com/';
 $whatsapp_link  =  get_field('whatsapp_link', 'option') ?: 'https://web.whatsapp.com/';
 $about_email  =  get_field('about_email', 'option') ?: 'info@mail.com';
+$footer_sub_heading = get_field('footer_sub_heading', 'option') ?: 'Want to start a project?';
 $footer_main_heading = get_field('footer_main_heading', 'option') ?: 'Lets Have a Chat';
 $head_right_button_link = get_field('head_right_button_link', 'option') ?: '#contact-wrapper';
 $target_url = '#contact-wrapper';
+$work_status = get_field('work_status', 'option');
+$messages = array(
+    'available'     => 'Available For Freelance',
+    'busy'          => 'Currently working on client projects.',
+    'not_available' => 'Not Available For Freelance.',
+    'vacation'      => 'Currently on vacation.',
+);
+$work_message = isset($messages[$work_status]) ? $messages[$work_status] : 'Want to start a project?';
 ?>
 <div class="main-footer">
 <div class="bg-shape">
@@ -21,13 +30,12 @@ $target_url = '#contact-wrapper';
 <div class="section-header">
 <div class="heading-left">
 
-<p class="wow fadeInUp" data-wow-delay=".3s"><a class="link modal-popup" href="#contact-wrapper"><?php echo get_field('footer_sub_heading', 'option'); ?></a></p>
-
-<a href="<?php echo ! empty( $head_right_button_link ) ? esc_url( $head_right_button_link ) : '#'; ?>" class="<?php if( $head_right_button_link === $target_url ) : ?>link modal-popup<?php endif; ?>"><h2 id="anim" class="section-title wow fadeInUp" data-wow-delay=".4s"><?php echo esc_html( $footer_main_heading ); ?></h2></a>
+<p class="wow fadeInUp" data-wow-delay=".3s"><a class="link modal-popup" href="#contact-wrapper"><?php echo esc_html( $footer_sub_heading ); ?></a></p>
+<a class="link modal-popup" href="#contact-wrapper"><h2 id="anim" class="section-title wow fadeInUp" data-wow-delay=".4s"><?php echo esc_html( $footer_main_heading ); ?></h2></a>
 
 </div>
-<div class="chat-mail wow fadeInRight" data-wow-delay=".5s">
-<a class="link modal-popup" href="#contact-wrapper"><?php echo get_field('footer_email_id', 'option'); ?><i class="fa-solid fa-arrow-right"></i></a>
+<div class="chat-mail wow fadeInUp" data-wow-delay=".5s">
+<a class="link modal-popup" href="#contact-wrapper"><?php echo esc_html( $about_email ); ?><i class="fa-solid fa-arrow-right"></i></a>
 <!-- <a class="btn tj-btn-primary modal-popup" href="#service-wrapper">Learn More <i class="fa-solid fa-arrow-right"></i></a> -->
 </div>
 </div>
@@ -42,7 +50,7 @@ $target_url = '#contact-wrapper';
 <div class="col">
 <div class="tj-footer-6-wrapper">
 <div class="tj-footer-6-left">
-<p class="tj-footer-6-paragraph">AVAILABLE FOR FREELANCE</p>
+<p class="tj-footer-6-paragraph <?php echo esc_attr($work_status); ?>"><?php echo esc_html($work_message); ?></p>
 </div>
 <div class="tj-footer-6-middle">
 <div class="footer-menu tj-footer-6-menu">
@@ -54,16 +62,16 @@ $target_url = '#contact-wrapper';
 <li><a href="<?php echo esc_html( $instagram_link ); ?>" target="_blank"><i class="fa-brands fa-instagram"></i>Instagram.</a></li>
 </ul>
 <ul class="mobile_social_icon">
-<li><a href="portfolio.html"><i class="fa-brands fa-whatsapp"></i></a></li>
-<li><a href="services.html"><i class="fa-brands fa-linkedin-in"></i></a></li>
-<li><a href="about.html"><i class="fa-brands fa-facebook"></i></a></li>
-<li><a href="contact.html"><i class="fa-brands fa-instagram"></i></a></li>
+<li><a href="<?php echo esc_html( $whatsapp_link ); ?>" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
+<li><a href="<?php echo esc_html( $linkedin_link ); ?>" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a></li>
+<li><a href="<?php echo esc_html( $facebook_link ); ?>" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+<li><a href="<?php echo esc_html( $instagram_link ); ?>" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
 </ul>
 </nav>
 </div>
 </div>
 <div class="tj-footer-6-right">
-<p class="tj-footer-6-copyright">© All rights reserved by <a href="index.html">Deepak Sherawat Web developer</a></p>
+<p class="tj-footer-6-copyright">© All rights reserved by <a href="<?php echo site_url(); ?>"><?php echo get_bloginfo('name'); ?></a></p>
 </div>
 </div>
 </div>
